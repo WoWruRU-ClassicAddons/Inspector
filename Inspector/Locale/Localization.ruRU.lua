4c169d45d1c5a74cb6efd66ce8c79d8ae4c3983c
M2us <Dead_Maus@mail.ru>
2017-06-12 19:40:34 +0300

    Added support ruRU locale

---
+++ Inspector/Locale/Localization.ruRU.lua
@@ -0,0 +1,45 @@
+
+-- Russion localization by Lichery
+
+if ( GetLocale() == "ruRU" ) then
+	----------
+	-- LUA
+	----------
+	BINDING_NAME_INSPECTOR_BINDING = "Осмотреть цель";
+	
+	INSPECTOR_LOAD = " Загружен!"
+	INSPECTOR_LOAD2 = "Введите /inspector для информации"
+
+	INSPECTOR_CMD1 = "/in - Осмотреть вашу текущую цель"
+	INSPECTOR_CMD2 = "/in <target's name> - Попытка осмотра указанной цели"
+	INSPECTOR_CMD3 = "Вы можете также привязать горячую клавишу ( 'I', например) для осмотра цели"
+	
+	INSPECTOR_PLAYER_LEVEL = "-го уровня"
+	INSPECTOR_PLAYER_GUILD = " - "
+	INSPECTOR_PLAYER_RANK = " (Звание "
+	INSPECTOR_PLAYER_0RANK = "Нет PvP Звания"
+
+	INSPECTOR_REQ_HONOR = "Запрос статистики чести..."
+	INSPECTOR_RE_REQ_HONOR = "Повтор запроса статистики чести..."
+	INSPECTOR_MOVE_CLOSER = "Подойдите ближе для просмотра статистики чести"
+	INSPECTOR_LIFETIME_0RANK = "Нет"
+	INSPECTOR_STATS_LOADED = "Статистика чести загружена"
+
+	INSPECTOR_BUTTON_HONOR = "Честь"
+	INSPECTOR_BUTTON_MODEL = "Модель"
+	
+	----------
+	-- XML
+	----------
+	INSPECTOR_TODAY = "Сегодня"
+	INSPECTOR_YESTERDAY = "Вчера"
+	INSPECTOR_THIS_WEEK = "За эту неделю"
+	INSPECTOR_LAST_WEEK = "За прошлую неделю"
+	INSPECTOR_LIFETIME = "Всего"
+	INSPECTOR_HK = "Почетные победы"
+	INSPECTOR_DK = "Бесчестные убийства"
+	INSPECTOR_HONOR = "Честь"
+	INSPECTOR_STAND = "Отношение"
+	INSPECTOR_RANK = "Высшее звание"
+	
+end
