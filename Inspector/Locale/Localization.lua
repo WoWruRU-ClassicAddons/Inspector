63571aa2a9443d1d86b2aba68e5c3382a1d1387c
M2us <Dead_Maus@mail.ru>
2017-06-12 19:36:01 +0300

    Initial commit

---
+++ Inspector/Locale/Localization.lua
@@ -0,0 +1,40 @@
+----------
+-- LUA
+----------
+
+BINDING_NAME_INSPECTOR_BINDING = "Inspect Target";
+
+INSPECTOR_LOAD = " Loaded!"
+INSPECTOR_LOAD2 = "Type /inspector for info"
+
+INSPECTOR_CMD1 = "/in - Inspect your current target"
+INSPECTOR_CMD2 = "/in <target's name> - Tries to inspect the target you specify (or the closest match)"
+INSPECTOR_CMD3 = "You can also bind a key ('i' for example) to inspect a target"
+
+INSPECTOR_PLAYER_LEVEL = " Level"
+INSPECTOR_PLAYER_GUILD = " of "
+INSPECTOR_PLAYER_RANK = " (Rank "
+INSPECTOR_PLAYER_0RANK = "No PvP Rank"
+
+INSPECTOR_REQ_HONOR = "Requesting Honor Stats..."
+INSPECTOR_RE_REQ_HONOR = "Re-Requesting Honor Stats..."
+INSPECTOR_MOVE_CLOSER = "Move Closer To See Honor Stats"
+
+INSPECTOR_LIFETIME_0RANK = "None"
+INSPECTOR_STATS_LOADED = "Honor Stats Loaded"
+
+INSPECTOR_BUTTON_HONOR = "Show Honor"
+INSPECTOR_BUTTON_MODEL = "Show Model"
+----------
+-- XML
+----------
+INSPECTOR_TODAY = "Today"
+INSPECTOR_YESTERDAY = "Yesterday"
+INSPECTOR_THIS_WEEK = "This Week"
+INSPECTOR_LAST_WEEK = "Last Week"
+INSPECTOR_LIFETIME = "Lifetime"
+INSPECTOR_HK = "Honorable Kills"
+INSPECTOR_DK = "Dishonorable Kills"
+INSPECTOR_HONOR = "Honor"
+INSPECTOR_STAND = "Standing"
+INSPECTOR_RANK = "Highest Rank"
